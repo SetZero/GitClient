944d45291b3cda248e6ffba0f02bccd386182caf
Sebastian <19Sebastian95@gmx.de>
2022-03-26 00:59:31 +0100

    update ui

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,42 @@
 extern crate git2;
+extern crate tui;
 
-use git2::Repository;
+use std::io;
+use tui::{
+    backend::CrosstermBackend,
+    widgets::{Widget, Block, Borders},
+    layout::{Layout, Constraint, Direction},
+    Terminal
+};
 
-fn main() {
-    println!("Starting Git Cli");
+use crossterm::{
+    event::{self, DisableMouseCapture, EnableMouseCapture, Event, KeyCode},
+    execute,
+    terminal::{disable_raw_mode, enable_raw_mode, EnterAlternateScreen, LeaveAlternateScreen},
+};
 
-    let repo = match Repository::init(".") {
-        Ok(repo) => repo,
-        Err(e) => panic!("failed to init: {}", e),
-    };
-    println!("Repo State: {:?}", repo.state());
+fn main() -> Result<(), io::Error> {
+    enable_raw_mode()?;
+    let stdout = io::stdout();
+    let backend = CrosstermBackend::new(stdout);
+    let mut terminal = Terminal::new(backend)?;
+
+    terminal.draw(|f| {
+        let size = f.size();
+        let block = Block::default()
+            .title("Block")
+            .borders(Borders::ALL);
+        f.render_widget(block, size);
+    })?;
+
+    disable_raw_mode()?;
+    execute!(
+        terminal.backend_mut(),
+        LeaveAlternateScreen,
+        DisableMouseCapture
+    )?;
+    terminal.show_cursor()?;
+
+
+    Ok(())
 }
